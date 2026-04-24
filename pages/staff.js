1e9dd5129ad4a48cfdd34d0d69d81343f2bfe1df
mackaylarodriguez <mackaylarodriguez6@gmail.com>
2026-04-23 20:39:13 -0700

    hover

--- pages/staff.js
+++ pages/staff.js
@@ -689,7 +689,7 @@ function WorkerDirectorySection({
                 const participant = participantForWorker(worker, participantByProfileId, participantByEmail);
                 const showMetrics = Boolean(participant);
                 return (
-                  <tr key={worker.id}>
+                  <tr key={worker.id} className="staffWorkerRow">
                     <td style={{ whiteSpace: "nowrap" }}>
                       <div style={{ fontWeight: 700 }}>
                         {worker.profileId ? (
